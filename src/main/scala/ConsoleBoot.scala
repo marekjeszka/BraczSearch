d937b71e674052569fa9f8a4be8174cb36899ba5
Marek Jeszka <marek.jeszka@gmail.com>
2016-12-17 19:50:32 +0100

    Console app implemented

--- src/main/scala/ConsoleBoot.scala
+++ src/main/scala/ConsoleBoot.scala
@@ -8,10 +8,27 @@ object ConsoleBoot extends App {
 //  private val ISBN = "9788380620438"
 
 
-  def programLoop() = {
-    val ISBN: String = StdIn.readLine("Please enter ISBN of book:\n")
-    val places = new CatalogScraper().getPlaces(conf.getString("braczsearch.link").format(ISBN))
-    places filter(_.available) foreach println
+  def programLoop(): Unit = {
+    while (true) {
+      val command: String = StdIn.readLine("*****\nPlease enter ISBN of book: (q + enter => quits)\n")
+      command match {
+        case "q" | "Q" => return
+        case _ =>
+          if (isISBN(command)) {
+            val places = new CatalogScraper().getPlaces(conf.getString("braczsearch.link").format(command))
+            val availablePlaces: List[Place] = places filter(_.available)
+            availablePlaces match {
+              case Nil => println(s"Sorry, could not find available locations for: $command")
+              case l => l foreach println
+            }
+          } else {
+            println("Please enter 10 or 13 digits only.")
+          }
+      }
+    }
   }
 
+  private def isISBN(command: String): Boolean = {
+    (command.length() == 10 || command.length == 13) && command.matches("\\d.*")
+  }
 }
