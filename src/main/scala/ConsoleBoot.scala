b9e24e7bd417030776226dce0003e432d23fd610
Marek Jeszka <marek.jeszka@gmail.com>
2016-12-21 08:10:34 -0800

    Switched to tail recursive

--- src/main/scala/ConsoleBoot.scala
+++ src/main/scala/ConsoleBoot.scala
@@ -1,45 +1,47 @@
 import com.typesafe.config.ConfigFactory
 
+import scala.annotation.tailrec
 import scala.io.StdIn
 
 object ConsoleBoot extends App {
-  val conf = ConfigFactory.load()
+  val searchLink = ConfigFactory.load().getString("braczsearch.link")
+
   programLoop()
 //  private val ISBN = "9788380620438"
 //	9788374805537
 
-  def programLoop(): Unit = {
-    var link : Option[String] = Option.empty
-    while (true) {
-      val command: String = StdIn.readLine("*****\nPlease enter ISBN of book: (h + enter => shows help)\n")
-      command match {
-        case "q" | "Q" => return
-        case "h" | "H" => println("Available commands:\nq - quits\nh - shows this help\nl - displays previously used link")
-        case "l" | "L" =>
-          link match {
-            case Some(l) => println(s"Previously used link: $l")
-            case None => println("Previous link is not available.")
+  @tailrec
+  def programLoop(link : String = ""): Unit = {
+    val command: String = StdIn.readLine("*****\nPlease enter ISBN of book: (h + enter => shows help)\n")
+    val (text, anotherLink) = command match {
+      case "q" | "Q" => return
+      case "h" | "H" =>
+        ("Available commands:\nq - quits\nh - shows this help\nl - displays previously used link", link)
+      case "l" | "L" =>
+        if (isEmpty(link))
+          ("Previous link is not available.", link)
+        else
+          (s"Previously used link: $link", link)
+      case _ =>
+        if (isISBN(command)) {
+          val newLink = searchLink.format(command)
+          val availablePlaces: List[Place] = new CatalogScraper().getPlaces(newLink) filter (_.available)
+          availablePlaces match {
+            case Nil => (s"Sorry, could not find available locations for: $command", newLink)
+            case l =>
+              ("This book is available at:\n" + l.mkString("\n"), newLink)
           }
-        case _ =>
-          if (isISBN(command)) {
-            link = Option.apply(conf.getString("braczsearch.link").format(command))
-            val places = new CatalogScraper().getPlaces(link.get)
-            val availablePlaces: List[Place] = places filter(_.available)
-            availablePlaces match {
-              case Nil => println(s"Sorry, could not find available locations for: $command")
-              case l =>
-                println("This book is available at:")
-                l foreach println
-            }
-          } else {
-            println("Please enter 10 or 13 digits only.")
-          }
-      }
-      println()
+        } else {
+          ("Please enter 10 or 13 digits only.", link)
+        }
     }
+    println(text + "\n")
+    programLoop(anotherLink)
   }
 
   private def isISBN(command: String): Boolean = {
     (command.length() == 10 || command.length == 13) && command.matches("\\d.*")
   }
+
+  private def isEmpty(x: String) = x == null || x.isEmpty
 }
